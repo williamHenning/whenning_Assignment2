f454c880c66a6d9f04ddd6e7f56c52aa567b12af
William Henning <whenning@uwo.ca>
2019-02-11 13:45:14 -0500

    First Commit

---
+++ Assets/__Scripts/Controller.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Lets you interact with the UI
+
+public class Controller : MonoBehaviour
+{
+    public float speed;
+    public Text scoreText;
+    Score points = new Score();
+    private Rigidbody rigidb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidb = GetComponent<Rigidbody>();
+        int score = 0;
+        SetScoreText(score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveH, 0.0f, moveV);
+        rigidb.AddForce(move * speed);
+    }
+
+    //Called when game object touches a trigger collider, will remove the game object we touch if it is a pick up
+    void OnTriggerEnter (Collider otherObj)
+    {
+        //Diamond pick up
+        if (otherObj.GetComponent<Renderer>().material.color == Color.cyan)
+        {
+            otherObj.gameObject.SetActive(false);
+            points.score += 4;
+            SetScoreText(points.score);        
+        }
+        //Gold pick up
+        else if (otherObj.GetComponent<Renderer>().material.color == Color.yellow)
+        {
+            otherObj.gameObject.SetActive(false);
+            points.score += 3;
+            SetScoreText(points.score);
+        }
+        //Silver pick up
+        else if (otherObj.GetComponent<Renderer>().material.color == Color.grey)
+        {
+            otherObj.gameObject.SetActive(false);
+            points.score += 2;
+            SetScoreText(points.score);
+        }
+        //Bronze pick up
+        else if (otherObj.GetComponent<Renderer>().material.color == Color.magenta)
+        {
+            otherObj.gameObject.SetActive(false);
+            points.score += 1;
+            SetScoreText(points.score);
+        }
+    }
+
+    public class Score
+    {
+        private int _score;
+        //Properties for scoring
+        public int score
+        {
+            get { return _score; }
+            set { _score = value; }
+        }
+    }
+    public void SetScoreText(int score)
+    {
+        scoreText.text = "Current Score: " + score.ToString();
+    }
+}
---
+++ Assets/__Scripts/SpawnPickups.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPickups : MonoBehaviour
+{
+    public int numPickups = 11;
+    public GameObject pickUp;
+    public float radius = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float angle = 360f / (float)numPickups;             //Determines even spacing between each pickup
+        Vector3 centerPos = new Vector3(0.0f, 1.5f, 0.0f);  //Centre of circle of pickups
+        
+        for (int i = 0; i < numPickups; i++)
+        {           
+            GameObject cube = Instantiate<GameObject>(pickUp);
+            cube.name = "Pickup " + i;
+
+            //Uses the RandomCircle function below to generate the position of each pick up object 
+            Vector3 pos = RandomCircle(centerPos, radius, angle * i);
+            cube.transform.position = pos;
+
+            //Chooses a random colour to assign each pickup, colour determining points
+            float val = Random.Range(1,5);
+            if (val==4)
+            {
+                cube.GetComponent<Renderer>().material.color = Color.cyan;
+            }
+            else if (val==3)
+            {
+                cube.GetComponent<Renderer>().material.color = Color.yellow;
+            }
+            else if (val==2)
+            {
+                cube.GetComponent<Renderer>().material.color = Color.grey;
+            }
+            else
+            {
+                cube.GetComponent<Renderer>().material.color = Color.magenta;
+            } 
+            
+        }
+    }
+
+    Vector3 RandomCircle(Vector3 center, float radius, float ang)
+    {
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y;
+        pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        return pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
